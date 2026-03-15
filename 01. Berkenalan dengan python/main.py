e7d2711bc76f9ce755d083b32313fca06c49922c
auraauvarosee <auraauvaroseendica@gmail.com>
2026-03-15 13:57:47 +0700

    upload

--- 01. Berkenalan dengan python/main.py
+++ 01. Berkenalan dengan python/main.py
@@ -2,3 +2,8 @@
 ini adalah komentar untuk beberapa baris """
 
 print("Hello World") # ini adalah print digunakan untuk menanpilkan output ke layar
+
+nama, umur = "Luis", 12
+
+print("Nama Saya = ", nama, "umur saya =", umur) 
+print("Umur Saya = ", umur)
